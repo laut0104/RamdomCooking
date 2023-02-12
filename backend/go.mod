42b05f9c34b437f80cd1d07e4ad8bfa984af80a0
Miyu Yano <78589722+laut0104@users.noreply.github.com>
2023-02-12 19:14:59 +0900

    データベース接続

--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,9 @@ require github.com/line/line-bot-sdk-go v7.8.0+incompatible
 
 require (
 	github.com/golang-jwt/jwt/v4 v4.4.3 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.3.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 )
 
@@ -17,8 +20,8 @@ require (
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.2.0 // indirect
-	golang.org/x/net v0.4.0 // indirect
-	golang.org/x/sys v0.3.0 // indirect
-	golang.org/x/text v0.5.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/net v0.6.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 )
--- backend/server.go
+++ backend/server.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	// "github.com/gin-gonic/gin"
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/line/line-bot-sdk-go/linebot"
@@ -19,78 +22,41 @@ import (
 func main() {
 	// インスタンスを作成
 	e := echo.New()
-	// bot, err := linebot.New(
-	// 	// os.Getenv("LINE_BOT_CHANNEL_SECRET"),
-	// 	// os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
-	// 	"28054097ea37d55e61e6d0589e5c869f",
-	// 	"e98a/3YltcPyahTpVzVWlqFIc7CiGjhlopb0ibpf83CoGK9nsK5YspJsbMOF3TDxhPfiDEeBeTyHNtJSk7Om3M64rgYB45EQKQGUhHoEJ/OvJ7USvstzsRKPrskxBrEIqdIZrWKX7xksHuNDrPW9hwdB04t89/1O/w1cDnyilFU=",
-	// )
-	// エラーに値があればログに出力し終了する
-	// if err != nil {
-	//     log.Fatal(err)
-	// }
-	// weatherパッケージパッケージから天気情報の文字列を取得する
-	// result, err := weather.GetWeather()
-	// // エラーに値があればログに出力し終了する
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
-	//     events, err := bot.ParseRequest(req)
-	//     if err != nil {
-	//         if err == linebot.ErrInvalidSignature {
-	//             w.WriteHeader(400)
-	//         } else {
-	//             w.WriteHeader(500)
-	//         }
-	//         return
-	//     }
-	//     for _, event := range events {
-	//         switch event.Type {
-	//         case linebot.EventTypeFollow:
-	//             message := "友達登録できたよ！ありがとう"
-	//             fmt.Printf("%v", event)
-	//             if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message)).Do(); err != nil {
-	//                 log.Print(err)
-	//             }
-
-	//         case linebot.EventTypeMessage:
-	//             switch message := event.Message.(type) {
-	//             case *linebot.TextMessage:
-	//                 if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-	//                     log.Print(err)
-	//                 }
-	//             case *linebot.StickerMessage:
-	//                 replyMessage := fmt.Sprintf(
-	//                     "sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
-	//                 if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
-	//                     log.Print(err)
-	//                 }
-	//             }
-	//         }
-	//     }
-	// })
-	// // This is just sample code.
-	// // For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
-	// if err := http.ListenAndServe("localhost:8080", nil); err != nil {
-	//     log.Fatal(err)
-	// }
 
 	// ミドルウェアを設定
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	// ルートを設定
-	e.GET("/", hello) // ローカル環境の場合、http://localhost:1323/ にGETアクセスされるとhelloハンドラーを実行する
+	e.GET("/", hello)
 	e.POST("/callback", line)
 
-	// サーバーをポート番号1323で起動
+	// サーバーをポート番号8080で起動
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
 // ハンドラーを定義
 func hello(c echo.Context) error {
+	// conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	conn, err := pgx.Connect(context.Background(), "postgres://root:password@postgres:5432/randomcooking")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	// var name string
+	// var lineuserid string
+	// err = conn.QueryRow(context.Background(), "test", "09999999").Scan(&name, &lineuserid)
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+	// 	os.Exit(1)
+	// }
+
+	// fmt.Println(name, lineuserid)
+
 	return c.String(http.StatusOK, "Hello, World!")
+
 }
 
 func line(c echo.Context) error {
@@ -100,11 +66,6 @@ func line(c echo.Context) error {
 		"28054097ea37d55e61e6d0589e5c869f",
 		"e98a/3YltcPyahTpVzVWlqFIc7CiGjhlopb0ibpf83CoGK9nsK5YspJsbMOF3TDxhPfiDEeBeTyHNtJSk7Om3M64rgYB45EQKQGUhHoEJ/OvJ7USvstzsRKPrskxBrEIqdIZrWKX7xksHuNDrPW9hwdB04t89/1O/w1cDnyilFU=",
 	)
-	// received, err := bot.ParseRequest(c.Request().(*standard.Request).Request)
-	// if err != nil {
-	//     log.Infof(cx, err.Error())
-	//     return c.JSON(http.StatusInternalServerError, err)
-	// }
 	events, err := bot.ParseRequest(c.Request())
 	if err != nil {
 		if err == linebot.ErrInvalidSignature {
@@ -114,7 +75,6 @@ func line(c echo.Context) error {
 			c.Response().WriteHeader(500)
 			return c.String(500, "Hello, World!")
 		}
-		// return c.String("Hello, World!")
 	}
 	for _, event := range events {
 		fmt.Printf("%v", event.Source)
@@ -141,10 +101,5 @@ func line(c echo.Context) error {
 			}
 		}
 	}
-	// This is just sample code.
-	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
-	// if err := http.ListenAndServe("localhost:8080", nil); err != nil {
-	// 	log.Fatal(err)
-	// }
 	return c.String(http.StatusOK, "Hello, World!")
 }
