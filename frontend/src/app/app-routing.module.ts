43438b9035f7882df7b9450a4347286b6694546b
Miyu Yano <78589722+laut0104@users.noreply.github.com>
2023-02-23 16:48:18 +0900

    メニュー一覧ページのモック画面を作成

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuListComponent } from './pages/menu-list/menu-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'menu-list',
+  component: MenuListComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
