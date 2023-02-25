d80799667f6a46c7b208b66043e64faefeb1f958
Miyu Yano <78589722+laut0104@users.noreply.github.com>
2023-02-26 04:07:25 +0900

    メニューの登録・取得ができる

--- docker/postgres/sql/db.sql
+++ docker/postgres/sql/db.sql
@@ -6,16 +6,16 @@ CREATE TABLE IF NOT EXISTS users(
     UNIQUE (lineuserid)
 );
 
--- INSERT INTO users VALUES("11112344", "test")
 
--- CREATE TABLE IF NOT EXISTS menus(
---     id serial NOT NULL, 
---     userid varchar(255) NOT NULL,
---     menuname varchar(255) NOT NULL,
---     recipes TEXT NOT NULL,
---     PRIMARY KEY (id),
---     CONSTRAINT menus_userid_fkey
---     FOREIGN KEY (userid)
---     REFERENCES users(id)
---     ON DELETE CASCADE ON UPDATE CASCADE
---     )
+CREATE TABLE IF NOT EXISTS menus(
+    id serial NOT NULL, 
+    userid integer NOT NULL,
+    menuname varchar(255) NOT NULL,
+    recipes TEXT[] NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT menus_userid_fkey
+    FOREIGN KEY (userid)
+    REFERENCES users(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+    )
