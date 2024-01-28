3c693ebbc4dca00a0885a465403607a5cd55bd13
Miyu Yano <78589722+laut0104@users.noreply.github.com>
2024-01-29 02:06:57 +0900

    デプロイ修正

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  ngrokSkipBrowserWarning: false,
   apiUrl: 'https://randomcooking-202401250200-smcsn3l24a-uc.a.run.app/',
   LIFF_ID: '1660690567-wegZZboy',
 };
